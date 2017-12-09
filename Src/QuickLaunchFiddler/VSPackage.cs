84a4525dd845fed56b7fda822cfe51aad94f462b
Greg Trevellick <gtrevellick@googlemail.com>
2017-12-09 14:44:12 +0000

    .

--- Src/QuickLaunchFiddler/VSPackage.cs
+++ Src/QuickLaunchFiddler/VSPackage.cs
@@ -14,21 +14,20 @@ using QuickLaunchFiddler.Commands;
 namespace QuickLaunchFiddler
 {
     [PackageRegistration(UseManagedResourcesOnly = true)]
-    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [InstalledProductRegistration(productName: "#110", productDetails: "#112", productId: Vsix.Version, IconResourceID = 400)] // Info on this package for Help/About
     [ProvideMenuResource("Menus.ctmenu", 1)]
-    [Guid(VSPackage.PackageGuidString)]
+    [Guid(PackageGuids.guidQuickButtonCommandPackageString)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    //[ProvideOptionPage(typeof(GeneralOptions), Vsix.Name, CommonConstants.CategorySubLevel, 0, 0, true)]
     public sealed class VSPackage : Package
     {
-        public const string PackageGuidString = "fbde4ce1-b539-4eb7-af0b-8b876f24a84d";
-
-        public VSPackage()
-        {
-            // Inside this method you can place any initialization code that does not require
-            // any Visual Studio service because at this point the package object is created but
-            // not sited yet inside Visual Studio environment. The place to do all the other
-            // initialization is the Initialize method.
-        }
+        //public VSPackage()
+        //{
+        //    // Inside this method you can place any initialization code that does not require
+        //    // any Visual Studio service because at this point the package object is created but
+        //    // not sited yet inside Visual Studio environment. The place to do all the other
+        //    // initialization is the Initialize method.
+        //}
 
         protected override void Initialize()
         {
