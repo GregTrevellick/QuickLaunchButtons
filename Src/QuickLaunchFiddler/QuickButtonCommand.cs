e5c583d35031f5b4ecdb1bf38fea53aaf60e6b92
Greg Trevellick <gtrevellick@googlemail.com>
2017-12-09 14:32:09 +0000

    .

--- Src/QuickLaunchFiddler/QuickButtonCommand.cs
+++ Src/QuickLaunchFiddler/QuickButtonCommand.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.Design;
 using System.Globalization;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
+using System.Diagnostics;
 
 namespace QuickLaunchFiddler
 {
@@ -26,16 +27,34 @@ namespace QuickLaunchFiddler
         /// </summary>
         private readonly Package package;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="QuickButtonCommand"/> class.
-        /// Adds our command handlers for menu (commands must exist in the command table file)
-        /// </summary>
-        /// <param name="package">Owner package, not null.</param>
+        ///// <summary>
+        ///// Initializes a new instance of the <see cref="QuickButtonCommand"/> class.
+        ///// Adds our command handlers for menu (commands must exist in the command table file)
+        ///// </summary>
+        ///// <param name="package">Owner package, not null.</param>
+        //private QuickButtonCommand(Package package)
+        //{
+        //    if (package == null)
+        //    {
+        //        throw new ArgumentNullException("package");
+        //    }
+
+        //    this.package = package;
+
+        //    OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+        //    if (commandService != null)
+        //    {
+        //        var menuCommandID = new CommandID(CommandSet, CommandId);
+        //        var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+        //        commandService.AddCommand(menuItem);
+        //    }
+        //}
+
         private QuickButtonCommand(Package package)
         {
             if (package == null)
             {
-                throw new ArgumentNullException("package");
+                throw new ArgumentNullException(nameof(package));
             }
 
             this.package = package;
@@ -43,10 +62,18 @@ namespace QuickLaunchFiddler
             OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
             if (commandService != null)
             {
-                var menuCommandID = new CommandID(CommandSet, CommandId);
-                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                CommandID menuCommandID = new CommandID(CommandSet, CommandId);
+                // Change to StartNotepad handler.  
+                MenuCommand menuItem = new MenuCommand(this.StartNotepad, menuCommandID);
                 commandService.AddCommand(menuItem);
             }
+        }
+
+        private void StartNotepad(object sender, EventArgs e)
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = "notepad.exe";
+            proc.Start();
         }
 
         /// <summary>
@@ -78,26 +105,26 @@ namespace QuickLaunchFiddler
             Instance = new QuickButtonCommand(package);
         }
 
-        /// <summary>
-        /// This function is the callback used to execute the command when the menu item is clicked.
-        /// See the constructor to see how the menu item is associated with this function using
-        /// OleMenuCommandService service and MenuCommand class.
-        /// </summary>
-        /// <param name="sender">Event sender.</param>
-        /// <param name="e">Event args.</param>
-        private void MenuItemCallback(object sender, EventArgs e)
-        {
-            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
-            string title = "QuickButtonCommand";
+        ///// <summary>
+        ///// This function is the callback used to execute the command when the menu item is clicked.
+        ///// See the constructor to see how the menu item is associated with this function using
+        ///// OleMenuCommandService service and MenuCommand class.
+        ///// </summary>
+        ///// <param name="sender">Event sender.</param>
+        ///// <param name="e">Event args.</param>
+        //private void MenuItemCallback(object sender, EventArgs e)
+        //{
+        //    string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+        //    string title = "QuickButtonCommand";
 
-            // Show a message box to prove we were here
-            VsShellUtilities.ShowMessageBox(
-                this.ServiceProvider,
-                message,
-                title,
-                OLEMSGICON.OLEMSGICON_INFO,
-                OLEMSGBUTTON.OLEMSGBUTTON_OK,
-                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
-        }
+        //    // Show a message box to prove we were here
+        //    VsShellUtilities.ShowMessageBox(
+        //        this.ServiceProvider,
+        //        message,
+        //        title,
+        //        OLEMSGICON.OLEMSGICON_INFO,
+        //        OLEMSGBUTTON.OLEMSGBUTTON_OK,
+        //        OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        //}
     }
 }
--- Src/QuickLaunchFiddler/QuickButtonCommandPackage.cs
+++ Src/QuickLaunchFiddler/QuickButtonCommandPackage.cs
@@ -12,23 +12,6 @@ using Microsoft.Win32;
 
 namespace QuickLaunchFiddler
 {
-    /// <summary>
-    /// This is the class that implements the package exposed by this assembly.
-    /// </summary>
-    /// <remarks>
-    /// <para>
-    /// The minimum requirement for a class to be considered a valid package for Visual Studio
-    /// is to implement the IVsPackage interface and register itself with the shell.
-    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-    /// to do it: it derives from the Package class that provides the implementation of the
-    /// IVsPackage interface and uses the registration attributes defined in the framework to
-    /// register itself and its components with the shell. These attributes tell the pkgdef creation
-    /// utility what data to put into .pkgdef file.
-    /// </para>
-    /// <para>
-    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
-    /// </para>
-    /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
     [ProvideMenuResource("Menus.ctmenu", 1)]
@@ -36,14 +19,8 @@ namespace QuickLaunchFiddler
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
     public sealed class QuickButtonCommandPackage : Package
     {
-        /// <summary>
-        /// QuickButtonCommandPackage GUID string.
-        /// </summary>
         public const string PackageGuidString = "fbde4ce1-b539-4eb7-af0b-8b876f24a84d";
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="QuickButtonCommand"/> class.
-        /// </summary>
         public QuickButtonCommandPackage()
         {
             // Inside this method you can place any initialization code that does not require
@@ -52,18 +29,10 @@ namespace QuickLaunchFiddler
             // initialization is the Initialize method.
         }
 
-        #region Package Members
-
-        /// <summary>
-        /// Initialization of the package; this method is called right after the package is sited, so this is the place
-        /// where you can put all the initialization code that rely on services provided by VisualStudio.
-        /// </summary>
         protected override void Initialize()
         {
             QuickButtonCommand.Initialize(this);
             base.Initialize();
         }
-
-        #endregion
     }
 }
