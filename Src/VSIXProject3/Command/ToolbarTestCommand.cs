4979f0af4745404813b476d693c5011756df65af
Greg Trevellick <gtrevellick@googlemail.com>
2017-12-10 00:55:41 +0000

    .

---
+++ Src/VSIXProject3/Command/ToolbarTestCommand.cs
@@ -0,0 +1,74 @@
+﻿using System.Globalization;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using static System.Environment;
+
+namespace VSIXProject3
+{
+    internal sealed class ToolbarTestCommand
+    {
+        public const int CommandId = 0x0100;
+
+        public static readonly Guid CommandSet = new Guid("ea775f15-152c-46d2-b8c1-7819c9fb5bff");
+
+        private readonly Package package;
+
+        private ToolbarTestCommand(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+
+            this.package = package;
+
+            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        public static ToolbarTestCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        public static void Initialize(Package package)
+        {
+            Instance = new ToolbarTestCommand(package);
+        }
+
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            string title = "ToolbarTestCommand";
+
+            VsShellUtilities.ShowMessageBox(
+                this.ServiceProvider,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+    }
+}
---
+++ Src/VSIXProject3/VSPackage.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace VSIXProject3
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]       
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(VSPackage.PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    public sealed class VSPackage : Package
+    {
+        public const string PackageGuidString = "462b1151-d39e-4099-b947-7702b6b60e9b";
+
+        public VSPackage()
+        {
+        }
+
+        protected override void Initialize()
+        {
+            ToolbarTestCommand.Initialize(this);
+            base.Initialize();
+        }
+
+    }
+}
