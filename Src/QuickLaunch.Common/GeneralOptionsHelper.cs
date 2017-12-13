64c0d054505e3a15ea589c57fc01e43cd03156f1
Greg Trevellick <gtrevellick@googlemail.com>
2017-12-13 12:33:33 +0000

    .

--- Src/QuickLaunch.Common/GeneralOptionsHelper.cs
+++ Src/QuickLaunch.Common/GeneralOptionsHelper.cs
@@ -108,10 +108,42 @@ namespace QuickLaunch.Common
 
         private static IList<string> GetSpecialFoldersPlusThirdPartyExePath(string executableFileToBrowseFor, string secondaryFilePathSegment)
         {
+            #region
+            //var allSpecialFolders = new List<String>();
+            //for (int i = 0; i <= 59; i++)
+            //{
+            //    if (i != 1 && i != 3 && i != 4 && i != 10 && i != 12 && i != 15 && i != 18 && i != 29 && i != 30 && i != 31 && i != 32 && i != 49 && i != 50 && i != 51 && i != 52)
+            //    {
+            //        var specialFolder = (Environment.SpecialFolder)i;
+            //        var initialFolderPath = Environment.GetFolderPath(specialFolder);
+            //        allSpecialFolders.Add(initialFolderPath);
+            //    }
+            //}
+            //allSpecialFolders.Sort();
+            #endregion
+
             var paths = new List<string>();
 
             if (!string.IsNullOrEmpty(executableFileToBrowseFor))
             {
+                var initialFolderPaths = new List<string>();
+
+                #region gregtLO add this section to OIA.sln
+                var pathVariable = Environment.GetEnvironmentVariable("path");
+                if (!string.IsNullOrEmpty(pathVariable))
+                {
+                    var pathVariables = pathVariable.Split(';');
+                    foreach (var path in pathVariables)
+                    {
+                        if (path.EndsWith(secondaryFilePathSegment))
+                        {
+                            var trimmedPath = path.Substring(0, path.Length - secondaryFilePathSegment.Length);
+                            initialFolderPaths.Add(trimmedPath);
+                        }
+                    }
+                }
+                #endregion
+
                 //set up array of the four special folders
                 var initialFolders = new List<InitialFolderType>
                 {
@@ -119,7 +151,6 @@ namespace QuickLaunch.Common
                     InitialFolderType.LocalApplicationData,
                     InitialFolderType.Windows
                 };
-                var initialFolderPaths = new List<string>();
                 foreach (var initialFolder in initialFolders)
                 {
                     var specialFolder = (Environment.SpecialFolder)initialFolder;
@@ -166,7 +197,7 @@ namespace QuickLaunch.Common
             return result;
         }
 
-        private static IEnumerable<string> DoubleUpForMultipleSecondaryFilePathSegments(IEnumerable<string> searchPaths, string secondaryFilePathSegment)//gregt unit test reqd
+        private static IEnumerable<string> DoubleUpForMultipleSecondaryFilePathSegments(IEnumerable<string> searchPaths, string secondaryFilePathSegment)//gregtLO unit test reqd
         {
             var nbrPaths = new List<string>();
 
