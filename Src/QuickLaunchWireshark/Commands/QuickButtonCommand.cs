7ef192596f093b468761c62bd74bb938f79ef736
Greg Trevellick <gtrevellick@googlemail.com>
2017-12-15 06:34:45 +0000

    .

---
+++ Src/QuickLaunchWireshark/Commands/QuickButtonCommand.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.ComponentModel.Design;
+using QuickLaunch.Common;
+
+namespace QuickLaunch.Wireshark.Commands
+{
+    internal class QuickButtonCommand
+    {
+        private readonly Package package;
+        private IServiceProvider ServiceProvider => this.package;
+        public const int CommandId = PackageIds.QuickButtonCommandId;
+        public static readonly Guid CommandSet = new Guid(PackageGuids.guidQuickButtonCommandPackageCmdSetString);
+        public static QuickButtonCommand Instance { get; private set; }
+
+        public static void Initialize(Package package)
+        {
+            Instance = new QuickButtonCommand(package);
+        }
+
+        private QuickButtonCommand(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException(nameof(package));
+            }
+
+            this.package = package;
+
+            var commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+
+            if (commandService != null)
+            {
+                var menuCommandId = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(InvokeApplication, menuCommandId);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        private void InvokeApplication(object sender, EventArgs e)
+        {
+            GeneralOptionsHelper.InvokeApplication(VSPackage.Options.ActualPathToExe);       
+        }
+    }
+}
---
+++ Src/QuickLaunchWireshark/Options/GeneralOptions.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel;
+using Microsoft.VisualStudio.Shell;
+using QuickLaunch.Common;
+
+namespace QuickLaunch.Wireshark.Options
+{
+    public class GeneralOptions : DialogPage
+    {
+        private const string CommonActualPathToExeOptionLabel = CommonConstants.ActualPathToExeOptionLabelPrefix + CommonConstants.WiresharkExeName;
+
+        [DisplayName(CommonActualPathToExeOptionLabel)]
+        [Description(CommonConstants.ActualPathToExeOptionDetailedDescription)]
+        public string ActualPathToExe { get; set; }
+
+        public override void LoadSettingsFromStorage()
+        {
+            base.LoadSettingsFromStorage();
+
+            if (string.IsNullOrEmpty(ActualPathToExe))
+            {
+                ActualPathToExe = GeneralOptionsHelper.GetActualPathToExe(secondaryFilePathSegment: "Wireshark", executableFileToBrowseFor: CommonConstants.WiresharkExeName + CommonConstants.DefaultExecutableFileSuffix, multipleSecondaryFilePathSegments: true);
+            }
+
+            previousActualPathToExe = ActualPathToExe;
+        }
+
+        private string previousActualPathToExe { get; set; }
+
+        protected override void OnApply(PageApplyEventArgs e)
+        {
+            var actualPathToExeChanged = false;
+
+            if (ActualPathToExe != previousActualPathToExe)
+            {
+                actualPathToExeChanged = true;
+                previousActualPathToExe = ActualPathToExe; 
+            }
+
+            if (actualPathToExeChanged)
+            {
+                if (!ArtefactsHelper.DoesActualPathToExeExist(ActualPathToExe))
+                {
+                    e.ApplyBehavior = ApplyKind.Cancel;
+
+                    var caption = new ConstantsForAppCommon().Caption;
+
+                    var filePrompterHelper = new FilePrompterHelper(caption, CommonConstants.WiresharkExeName);
+
+                    var persistOptionsDto = filePrompterHelper.PromptForActualExeFile(ActualPathToExe);
+
+                    if (persistOptionsDto.Persist)
+                    {
+                        PersistVSToolOptions(persistOptionsDto.ValueToPersist);
+                    }
+                }
+            }
+
+            base.OnApply(e);
+        }
+
+        public void PersistVSToolOptions(string fileName)
+        {
+            VSPackage.Options.ActualPathToExe = fileName;
+            VSPackage.Options.SaveSettingsToStorage();
+        }
+    }
+}
