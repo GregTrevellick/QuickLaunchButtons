fa53672f22f2c3818a9b93fbc1f17f3c4f5876ab
Greg Trevellick <gtrevellick@googlemail.com>
2018-11-12 08:07:02 +0000

    .

--- Src/QuickLaunchFiddler/Commands/InvokerHelper.cs
+++ Src/QuickLaunchFiddler/Commands/InvokerHelper.cs
@@ -55,8 +55,8 @@ namespace QuickLaunch.Fiddler.Commands
                 var generalOptions = await GeneralOptions.GetLiveInstanceAsync();
                 generalOptions.ActualPathToExe = fileName;
                 await generalOptions.SaveAsync();
-                await generalOptions.LoadAsync();/////////////////////////////////////////////////////////////////////this seems to be needed for the new options to load the 2nd time round
-                                                  //////////////////////////// GREGT NOLONGER TRUE ?????????????
+                ///////////////////////////////await generalOptions.LoadAsync();/////////////////////////////////////////////////////////////////////this seems to be needed for the new options to load the 2nd time round
+                  //////////////////////////// GREGT NOLONGER TRUE ?????????????
             });
         }
 
