c9c062aa36e80c1d96e042c3b126047dcea1c63b
Greg Trevellick <gtrevellick@googlemail.com>
2018-11-12 21:57:29 +0000

    .

--- Src/QuickLaunchFiddler/Options/BaseOptionPage.cs
+++ Src/QuickLaunchFiddler/Options/BaseOptionPage.cs
@@ -26,7 +26,7 @@ namespace QuickLaunch.Fiddler.Options
 
         public override void SaveSettingsToStorage()
         {
-            //_model.Save();
+            //////_model.Save();
             base.SaveSettingsToStorage();
         }
 
